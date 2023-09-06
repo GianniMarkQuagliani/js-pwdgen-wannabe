0bb9b2915446ab48a46eac853c3efa2b22b1bb3f
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-06 13:38:10 +0200

    [add] console.log, for testing

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-//Pseudo codice
-
 /* 
+(Pseudo codice)
 Inizia
 1. Chiedo all'utente il suo nome
 2. Chiedo all'utente il suo cognome
 3. Chiedo all'utente il suo colore preferito 
 4. Combino le informazioni: nome + cognome + colore preferito + numero (in questo caso è 23)
 5. Stampo il tutto sulla pagina
+Fine
 */
 
-
-
+//1. Chiedo all'utente il suo nome
+console.log('ciao mondo');
