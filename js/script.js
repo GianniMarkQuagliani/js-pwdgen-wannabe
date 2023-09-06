cb7b285ff9f49aabd0996cf2373e36111ea2721e
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-06 13:32:24 +0200

    [add] psedudo codice

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Pseudo codice
+
+/* 
+Inizia
+1. Chiedo all'utente il suo nome
+2. Chiedo all'utente il suo cognome
+3. Chiedo all'utente il suo colore preferito 
+4. Combino le informazioni: nome + cognome + colore preferito + numero (in questo caso è 23)
+5. Stampo il tutto sulla pagina
+*/
+
+
+
